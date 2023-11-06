55b0ebf96cd8088a4c239505db72f7c1f0650c95
abdullah <abdullahalhidany@gmail.com>
2023-11-06 13:26:11 +0300

    change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import  ReactDOM  from "react";
 
-import App from '/App';
-
-ReactDOM.render(<App/>,document.getElementById('root'));
+// import App from './App';
+ReactDOM.render(<h1>hhh</h1>, document.getElementById("root"));
